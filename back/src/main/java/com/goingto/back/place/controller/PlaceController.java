50cec4c6e1524454edd49cc76ffdd4432f874d30
jiwon111 <dreamjiwon@naver.com>
2022-08-10 00:00:18 +0900

    cors

--- back/src/main/java/com/goingto/back/place/controller/PlaceController.java
+++ back/src/main/java/com/goingto/back/place/controller/PlaceController.java
@@ -5,6 +5,7 @@ import com.goingto.back.place.service.PlaceService;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/place")
